169c3e56023f2fccabc32f3025d7ef1650a7a645
abel-cabral <jcabral@id.uff.br>
2019-08-05 12:17:41 -0300

    Melhorias na responsividade mobile e correcoes de errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,40 +4,47 @@ import {FormControl} from '@angular/forms';
 import AWN from 'awesome-notifications';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  longUrl = new FormControl();
-  botoesManipucao = false;
-  loading = false;
+    longUrl = new FormControl();
+    botoesManipucao = false;
+    loading = false;
 
-  constructor(private service: AppService) {
-  }
+    constructor(private service: AppService) {
+    }
 
-  onSubmit(): void {
-    this.loading = true;
-    const body = {
-      longUrl: this.longUrl.value
-    };
+    onSubmit(): void {
+        if (!this.longUrl.value) {
+            new AWN().warning('Informe um URL', {durations: {success: 1300}});
+            return;
+        }
 
-    this.service.requestShortUrl(body).subscribe(res => {
-      this.longUrl.patchValue(res.shortUrl);
-      this.botoesManipucao = true;
-    }, error => alert('Algo não ocorreu como deveria :/'));
-    this.loading = false;
-  }
+        this.loading = true;
+        const body = {longUrl: this.longUrl.value};
 
-  toCopy(): void {
-    const copyText = document.getElementById('shortUrl') as HTMLInputElement;
-    copyText.select();
-    document.execCommand('copy');
-    new AWN().success('Link copiado!', {durations: {success: 2200}});
-  }
+        this.service.requestShortUrl(body).subscribe(res => {
+            this.longUrl.patchValue(res.shortUrl);
+            this.botoesManipucao = true;
+            this.loading = false;
+        }, error => {
+            new AWN().error('Algo não ocorreu como deveria :/', {durations: {success: 1300}});
+            this.loading = false;
+        });
 
-  reset(): void {
-    this.longUrl.patchValue('');
-    this.botoesManipucao = false;
-  }
+    }
+
+    toCopy(): void {
+        const copyText = document.getElementById('shortUrl') as HTMLInputElement;
+        copyText.select();
+        document.execCommand('copy');
+        new AWN().success('Link copiado!', {durations: {success: 1700}});
+    }
+
+    reset(): void {
+        this.longUrl.patchValue('');
+        this.botoesManipucao = false;
+    }
 }
